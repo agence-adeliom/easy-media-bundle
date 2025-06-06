4fd0cf263c2f486e9345b39c1ba76e4dd3f342b3
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2025-06-06 17:04:11 +0200

    fix deprecation

--- src/DependencyInjection/EasyMediaExtension.php
+++ src/DependencyInjection/EasyMediaExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class EasyMediaExtension extends Extension implements PrependExtensionInterface
 {
