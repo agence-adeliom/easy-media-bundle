0815fd357ca255d0812e7a0b0f3d9b0357bb1f2c
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2025-11-05 00:17:59 +0100

    fix the image loading when fetching EasyMediaType Doctrine type

--- src/EasyMediaBundle.php
+++ src/EasyMediaBundle.php
@@ -5,11 +5,22 @@ declare(strict_types=1);
 namespace Adeliom\EasyMediaBundle;
 
 use Adeliom\EasyMediaBundle\DependencyInjection\EasyMediaExtension;
+use Adeliom\EasyMediaBundle\Types\EasyMediaType;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EasyMediaBundle extends Bundle
 {
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Initialize the EasyMediaType with the container
+        if ($this->container !== null) {
+            EasyMediaType::setContainer($this->container);
+        }
+    }
+
     /**
      * @return ExtensionInterface|null The container extension
      */
